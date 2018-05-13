1abd7f9b4fc262be47fe90b9a496aa679f991c25
Djordje Dimovic <33665996+dimovicdj@users.noreply.github.com>
2018-05-13 14:34:52 +0200

    Delete view.cpp

--- view.cpp
+++
@@ -1,8 +0,0 @@
-#include "view.h"
-
-View::View() {
-    this->setFixedSize(QSize(1280, 760));
-    // Iskljucujemo prikaz skrolova
-    this->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    this->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-}
